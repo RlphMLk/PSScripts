4116f3612fe77b4052642fa57fa96cef63f49b71
RlphMLk <100044834+RlphMLk@users.noreply.github.com>
2022-02-21 13:24:47 +0100

    Update

---
+++ Create-VM.ps1
@@ -0,0 +1,19 @@
+﻿param(
+    $Name
+    )
+
+
+
+$vhdx = "F:\Hyper-V\Virtual Hard Disks\" + $Name + ".vhdx"
+Copy-Item "F:\Hyper-V\Virtual Hard Disks\Master-Win10-21H2.vhdx" $vhdx
+#$VM = New-VM -Name $Name -MemoryStartupBytes 6GB -NewVHDPath $vhdx -NewVHDSizeBytes 120GB -Generation 2 -SwitchName "Default switch"
+$VM = New-VM -Name $Name -MemoryStartupBytes 6GB -VHDPath $vhdx -Generation 2 -SwitchName "Default switch"
+
+$VM | Set-VM -AutomaticCheckpointsEnabled $false -CheckpointType Disabled
+$VM | Set-VMProcessor -ExposeVirtualizationExtensions $true -Count 2
+$VM | Set-VMMemory -DynamicMemoryEnabled $false
+
+$owner = Get-HgsGuardian UntrustedGuardian
+$kp = New-HgsKeyProtector -Owner $owner -AllowUntrustedRoot
+Set-VMKeyProtector -VMName $Name -KeyProtector $kp.RawData
+Enable-VMTPM -VMName $Name
